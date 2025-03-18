86465d674c9eec82d3d8d5d56e04e97dc5d9db17
Deegant Mistry <m.deegant@gmail.com>
2025-03-18 01:21:13 -0400

    Uptime with formatting

--- src/format.cpp
+++ src/format.cpp
@@ -1,6 +1,6 @@
-#include <string>
-
 #include "format.h"
+
+#include <string>
 
 using std::string;
 
@@ -8,4 +8,13 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  string result = "";
+  int h, m, s;
+
+  h = seconds / 3600;
+  m = (seconds % 3600) / 60;
+  s = seconds % 60;
+  return ((h < 10 ? "0" : "") + std::to_string(h) + ":" + (m < 10 ? "0" : "") +
+          std::to_string(m) + ":" + (s < 10 ? "0" : "") + std::to_string(s));
+}
